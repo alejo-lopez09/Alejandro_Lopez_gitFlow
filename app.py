760310c230e07c17a8d26be0845fab50c1ef5c19
Alejo Lopez <alejitolopez19900109@gmaol.com>
2026-02-28 12:03:42 -0500

    fix: corregir mensaje del archivo .py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print ("Holis")
 def saludar(nombre):
-    return f"hola {nombre}, bienvenido al sistema"
+    return f"hola {nombre}, bienvenido al sistema actualizado"
 print (saludar("Alejandro"))
